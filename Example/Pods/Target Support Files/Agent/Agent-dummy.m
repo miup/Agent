7275ae87a390fec55b6ae4b5a6c71b119318651d
miup <contact@miup.blue>
2017-10-12 10:29:23 +0900

    rm Pods/

--- Example/Pods/Target Support Files/Agent/Agent-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Agent : NSObject
-@end
-@implementation PodsDummy_Agent
-@end
--- Example/Pods/Target Support Files/Pods-Agent_Example/Pods-Agent_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Agent_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Agent_Example
-@end
